652f2b41d9972a7ac9dc68fa014f46bc61db0aaf
akanksha <akanksha439@gmail.com>
2021-01-16 12:22:54 +0530

    remove movies from favourite

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,8 @@
 //action types
 export const ADD_MOVIES='ADD_MOVIES';
+export const ADD_FAVOURITE='ADD_FAVOURITE';
+export const REMOVE_FROM_FAVOURITES='REMOVE_FROM_FAVOURITES';
+
 
 //action creators
 export function addMovies(movies){
@@ -7,4 +10,16 @@ export function addMovies(movies){
         type:ADD_MOVIES,
         movies
     }
+}
+export function addFavourites(movie){
+    return{
+        type:ADD_FAVOURITE,
+        movie
+    };
+}
+export function removeFromFavourites(movie){
+    return{
+        type:REMOVE_FROM_FAVOURITES,
+        movie
+    };
 }
