ad2b5fac3411c19a4f4f959cb20a375ab77a6bac
akanksha <akanksha439@gmail.com>
2021-01-17 12:23:22 +0530

    add combineReducers() method

--- src/components/App.js
+++ src/components/App.js
@@ -19,9 +19,9 @@ class App extends React.Component{
   }
 
   isMovieFavourite=(movie)=>{
-    const { favourites } = this.props.store.getState();
+    const { movies } = this.props.store.getState();
 
-    const index =favourites.indexOf(movie);
+    const index =movies.favourites.indexOf(movie);
     if (index !==-1) {
       return true;
     }
@@ -33,7 +33,8 @@ class App extends React.Component{
     this.props.store.dispatch(setShowFavourites(val));
   }
   render(){
-    const {list,favourites,showFavourites}=this.props.store.getState();
+    const {movies}=this.props.store.getState();
+    const {list,favourites,showFavourites}=movies;
     const dispalyMovies=showFavourites?favourites:list;
     console.log('RENDER',this.props.store.getState());
     return (
